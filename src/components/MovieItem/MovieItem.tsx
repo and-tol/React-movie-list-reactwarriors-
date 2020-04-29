103585dd7497342676e9dba3aecc9a0fcf7d9318
andreitol <tolstoi.a@gmail.com>
2020-04-29 13:39:21 +0300

    feat: implement add movie to will watch

--- src/components/MovieItem/MovieItem.tsx
+++ src/components/MovieItem/MovieItem.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import { Movie } from './../../data/moviesData';
 
 interface MovieItemProps {
-  movie: {};
-  key: number;
+  movie: Movie;
+  removeMovie: func;
+  addMovieToWillWatch: func;
 }
 
 export const MovieItem: React.FC<MovieItemProps> = (props) => {
-  const { movie, removeMovie } = props;
+  const { movie, removeMovie, addMovieToWillWatch } = props;
+  const url: string = "https://image.tmdb.org/t/p/w500"
 
   return (
     <div className='card' style={{ width: '100%' }}>
-      <img className='card-img-top' src={movie.poster_path} />
+      <img className='card-img-top' src={`${url}${movie.poster_path || movie.poster_path}`} />
       <div className='card-body'>
         <h6 className='card-title'>{movie.title}</h6>
         <div className='d-flex justify-content-between align-items-center'>
           <p className='mb-0'>Rating: {movie.vote_average}</p>
-          <button onClick={removeMovie.bind(null, movie)} type='button' href='#' className='btn btn-primary'>
+          <button onClick={addMovieToWillWatch.bind(null, movie)} type='button' className='btn btn-success'>
             Will Watch
+          </button>
+          <button onClick={removeMovie.bind(null, movie)} type='button' className='btn btn-warning'>
+            Remove
           </button>
         </div>
       </div>
--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -5,24 +5,36 @@ import { MovieItem } from '../MovieItem/MovieItem';
 
 interface List {
   movies: Array<Movie>;
+  moviesWillWatch: [];
 }
+
+// UI = fn(state, props)
 
 export class MovieList extends Component<{}, List> {
   constructor() {
     super();
     this.state = {
       movies: moviesData,
+      moviesWillWatch: [],
     };
 
     // this.removeMovie = this.removeMovie.bind(this);
   }
 
-  removeMovie = (movie) => {
-    const updateMovies = this.state.movies.filter((item) => item.id !== movie.id);
-    this.state.movies = updateMovies;
+  removeMovie = (movie: Movie): void => {
+    // const updateMovies = this.state.movies.filter((item) => item.id !== movie.id);
+    // this.state.movies = updateMovies;
 
     this.setState({
-      movies: updateMovies,
+      movies: this.state.movies.filter((item) => item.id !== movie.id),
+    });
+  };
+
+  addMovieToWillWatch = (movie: Movie): void => {
+    // const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+    // updateMoviesWillWatch.push(movie)
+    this.setState({
+      moviesWillWatch: [...this.state.moviesWillWatch, movie],
     });
   };
 
@@ -31,12 +43,22 @@ export class MovieList extends Component<{}, List> {
       <div className='container'>
         <div className='row'>
           <div className='col-9'>
-            {this.state.movies.map((movie) => {
-              return <MovieItem key={movie.id} movie={movie} removeMovie={this.removeMovie} />;
-            })}
+            <div className='row'>
+              {this.state.movies.map((movie) => {
+                return (
+                  <div key={movie.id} className='col-6 mb-4'>
+                    <MovieItem
+                      movie={movie}
+                      removeMovie={this.removeMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="col-3">
-            <p>Will Watch 0</p>
+          <div className='col-3'>
+            <p>Will Watch {this.state.moviesWillWatch.length}</p>
           </div>
         </div>
       </div>
